07f92d051f0b12e69349c996bcf8df13546330db
drewg73 <drew.garbenis@icloud.com>
2024-07-15 18:05:57 -1000

    changed window.py to graphics.py

--- graphics.py
+++ graphics.py
@@ -4,10 +4,7 @@ class Window():
 	def __init__(self, width, height):
 		self.__root = Tk()
 		self.__root.title("Maze Solver")
-		self.__root.geometry(f"{width}x{height}")
-		self.__root.minsize(width, height)
-		self.__root.maxsize(width, height)
-		self.canvas = Canvas(self.__root, {"bg": "white"})
+		self.canvas = Canvas(self.__root, bg="black", height=height, width=width)
 		self.canvas.pack(fill=BOTH, expand=1)
 		self.is_running = False
 		self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -19,7 +16,8 @@ class Window():
 	def wait_for_close(self):
 		self.is_running = True
 		while self.is_running:
-			self.redraw()	
-	
+			self.redraw()
+		print("window closed...")
+			
 	def close(self):
 		self.is_running = False
