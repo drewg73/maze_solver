62c5d8dbffd7951bd4d75c4ecf97c9baa1f2efb5
drewg73 <drew.garbenis@icloud.com>
2024-07-15 18:21:03 -1000

    update main.py and graphics.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from graphics import Window
+from graphics import *
 
 def main():
 	win = Window(800, 600)
+	red_line = Line(Point(50, 50), Point(100, 50))
+	blue_line = Line(Point(100, 50), Point(100, 200))
+	win.draw_line(red_line, "red")
+	win.draw_line(blue_line, "blue")
 	win.wait_for_close()
 
 if __name__ == "__main__":
