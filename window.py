570e99cea52a944422ab24c2c4842c986ef5650b
drewg73 <drew.garbenis@icloud.com>
2024-07-15 18:00:42 -1000

    add main.py and .gitignore

--- window.py
+++ window.py
@@ -2,18 +2,19 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
 	def __init__(self, width, height):
-		self.width = width
-		self.height = height
 		self.__root = Tk()
 		self.__root.title("Maze Solver")
+		self.__root.geometry(f"{width}x{height}")
+		self.__root.minsize(width, height)
+		self.__root.maxsize(width, height)
 		self.canvas = Canvas(self.__root, {"bg": "white"})
-		self.canvas.pack(fill=BOTH, width=self.width, height=self.height, expand=1)
+		self.canvas.pack(fill=BOTH, expand=1)
 		self.is_running = False
 		self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
 	def redraw(self):
-		self.root.update_idletasks()
-		self.root.update()
+		self.__root.update_idletasks()
+		self.__root.update()
 
 	def wait_for_close(self):
 		self.is_running = True
@@ -22,4 +23,3 @@ class Window():
 	
 	def close(self):
 		self.is_running = False
-
