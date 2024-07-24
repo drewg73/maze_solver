cdcf6210833f4082cab47ef3856dcf75dd1a74cc
drewg73 <drew.garbenis@icloud.com>
2024-07-23 17:56:08 -1000

    add cell.py, update graphics.py and main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from graphics import *
+from graphics import Window
+from cell import Cell
+
 
 def main():
-	win = Window(800, 600)
-	red_line = Line(Point(50, 50), Point(100, 50))
-	blue_line = Line(Point(100, 50), Point(100, 200))
-	win.draw_line(red_line, "red")
-	win.draw_line(blue_line, "blue")
-	win.wait_for_close()
+    win = Window(800, 600)
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
+    win.wait_for_close()
 
 if __name__ == "__main__":
-	main()
+  main()
