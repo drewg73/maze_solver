1702124f5d0968e8d8dcb0340677d094bdaadb81
Drew Garbenis <70610167+drewg73@users.noreply.github.com>
2024-07-26 13:04:38 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
 from graphics import Window
-from cell import Cell
-
+from maze import Maze
 
 def main():
-    win = Window(800, 600)
-
-    c1 = Cell(win)
-    c1.has_right_wall = False
-    c1.draw(50, 50, 100, 100)
-
-    c2 = Cell(win)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(100, 50, 150, 100)
-
-    c1.draw_move(c2)
-
-    c3 = Cell(win)
-    c3.has_top_wall = False
-    c3.has_right_wall = False
-    c3.draw(100, 100, 150, 150)
-
-    c2.draw_move(c3)
-
-    c4 = Cell(win)
-    c4.has_left_wall = False
-    c4.draw(150, 100, 200, 150)
-
-    c3.draw_move(c4, True)
-
+    win = Window((100 + 50 * 10), (100 + 50 * 10))
+    m1 = Maze(50, 50, 10, 10, 50, 50, win, 10093409)
+    m1.solve()
     win.wait_for_close()
 
 if __name__ == "__main__":
-  main()
+    main()
